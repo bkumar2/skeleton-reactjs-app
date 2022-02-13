3f170e965204652028ea0c7e852e28d3ad8e0673
Bipul Kumar <bipul_kumar@intuit.com>
2022-02-13 22:47:56 +0530

    rich text editor

--- src/ui/FormDemo.jsx
+++ src/ui/FormDemo.jsx
@@ -1,6 +1,9 @@
+import { useRef } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Editor } from '@tinymce/tinymce-react';
 
 function FormDemo(props) {
+    const editorRef = useRef(null);
     return <Form>
         <Container>
             <Row>
@@ -24,9 +27,33 @@ function FormDemo(props) {
                 </Col>
             </Row>
             <Row>
-                <Col><Button variant="primary" type="submit">
-                    Submit
-                </Button></Col>
+                <Col>
+                    <Editor
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        initialValue="<p>This is the initial content of the editor.</p>"
+                        init={{
+                            height: 500,
+                            menubar: false,
+                            plugins: [
+                                'advlist autolink lists link image charmap print preview anchor',
+                                'searchreplace visualblocks code fullscreen',
+                                'insertdatetime media table paste code help wordcount'
+                            ],
+                            toolbar: 'undo redo | formatselect | ' +
+                                'bold italic backcolor | alignleft aligncenter ' +
+                                'alignright alignjustify | bullist numlist outdent indent | ' +
+                                'removeformat | help',
+                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                        }}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Col>
             </Row>
         </Container>
     </Form>;
