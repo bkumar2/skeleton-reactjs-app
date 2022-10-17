f5fcbfce656fe10e1f04385c8e3d9add5c8eb3b4
Bipul Kumar <bipul.kumar@gmail.com>
2022-10-17 09:23:59 +0530

    DnD sorting

--- src/ui/DnDDemo.jsx
+++ src/ui/DnDDemo.jsx
@@ -1,57 +1,98 @@
 import { useState } from "react";
-import { Col, Row } from "react-bootstrap";
-import { useDrag, useDrop } from 'react-dnd';
+import { useDrag, useDrop } from "react-dnd";
 
-function DraggableText(props) {
-    const [{ isDragging }, drag] = useDrag(() => ({
-        type: props.type,
-        item: { text: props.text },
-        collect: monitor => ({
-            isDragging: !!monitor.isDragging(),
+function DragDropItem(props) {
+    let ITEM = { index: props.index, text: props.text };
+    console.log("ITEM", ITEM);
+
+    const [{ isOver }, drop] = useDrop(
+        () => ({
+            accept: "item",
+            drop: (item) => {
+                console.log("received drop of:", item.index, item.text);
+                props.onDrop(item, ITEM);
+            },
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver(),
+            }),
         }),
-    }));
-    return <div ref={drag} style={{
-        opacity: isDragging ? 0.5 : 1,
-        backgroundColor: "black",
-        color: "white",
-        cursor: "pointer",
-        padding: "0 1em",
-        fontSize: "2em",
-        margin: "1em 0"
-    }}>{props.text}</div>;
-}
+        [ITEM]
+    );
 
-function DroppableContainer(props) {
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: props.accept,
-        drop: (item) => {
-            console.log("received drop:", item.text);
-            props.onChange(item.text);
-        },
-        collect: monitor => ({
-            isOver: !!monitor.isOver(),
+    const [collected, drag] = useDrag(
+        () => ({
+            type: "item",
+            item: ITEM,
         }),
-    }));
-    return <Col ref={drop} style={{ backgroundColor: props.color }}>
-        {props.data.map((text, i) => <DraggableText type={props.type} key={i} text={text} />)}
-    </Col>
-}
+        [ITEM]
+    );
 
-function move(text, arrayIndex, arrays, setArrays) {
-    let sourceArray = arrays[(arrayIndex + 1) % 2].filter((item) => item !== text);
-    let targetArray = [text, ...arrays[arrayIndex]];
-    let newArrays = [];
-    newArrays[arrayIndex] = targetArray;
-    newArrays[(arrayIndex + 1) % 2] = sourceArray;
-    setArrays(newArrays);
-    console.log("Moving to arrayIndex:", arrayIndex, " result ", newArrays);
+    return (
+        <div ref={drop}>
+            <div
+                id={props.index}
+                ref={drag}
+                style={{
+                    backgroundColor: isOver ? "gray" : "white",
+                    borderTop: isOver ? "3px yellow solid" : null,
+                    color: "black",
+                    cursor: "pointer",
+                    padding: "0 1em",
+                    fontSize: "2em",
+                    margin: "0.5em",
+                }}
+            >
+                {props.text}
+            </div>
+        </div>
+    );
 }
 
 function DnDDemo(props) {
-    const [arrays, setArrays] = useState([["A", "B", "C"], ["1", "2"]]);
-    return <Row>
-        <DroppableContainer type="left" accept="right" data={arrays[0]} onChange={(text) => move(text, 0, arrays, setArrays)} color="orange" />
-        <DroppableContainer type="right" accept="left" data={arrays[1]} onChange={(text) => move(text, 1, arrays, setArrays)} color="skyblue" />
-    </Row >;
+    const [array, setArray] = useState(["A", "B", "C", "D", "E"]);
+    console.log("array", array);
+    return (
+        <div
+            style={{
+                backgroundColor: "black",
+                width: "50%",
+                padding: "0.5em",
+            }}
+        >
+            {array.map((arrayElement, index) => (
+                <DragDropItem
+                    index={index}
+                    text={arrayElement}
+                    key={index}
+                    onDrop={(topItem, bottomItem) => {
+                        console.log("dropped", topItem, "on", bottomItem);
+                        let newArray = [...array];
+                        newArray.splice(topItem.index, 1);
+                        console.log(
+                            "newArray remove",
+                            topItem.index,
+                            topItem.text
+                        );
+                        newArray.splice(
+                            topItem.index < bottomItem.index
+                                ? bottomItem.index - 1
+                                : bottomItem.index,
+                            0,
+                            topItem.text
+                        );
+                        console.log(
+                            "newArray insert",
+                            topItem.index < bottomItem.index
+                                ? bottomItem.index - 1
+                                : bottomItem.index,
+                            topItem.text
+                        );
+                        console.log("newArray", newArray);
+                        setArray(newArray);
+                    }}
+                />
+            ))}
+        </div>
+    );
 }
 export default DnDDemo;
