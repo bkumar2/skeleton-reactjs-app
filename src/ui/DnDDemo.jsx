de5cc15c7d8f499b58f212dbbae658dfdf159041
Bipul Kumar <bipul.kumar@gmail.com>
2022-11-14 21:47:11 +0530

    drag drop

--- src/ui/DnDDemo.jsx
+++ src/ui/DnDDemo.jsx
@@ -1,24 +1,16 @@
 import { useRef, useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
 
-const HOVER_POSITION = {
-    TOP: 1,
-    BOTTOM: -1,
-    NONE: 0,
-};
-
 function DragDropItem(props) {
     const divRef = useRef(null);
-    const [hoverPosition, setHoverPosition] = useState(HOVER_POSITION.NONE);
     let ITEM = { index: props.index, text: props.text };
 
-    const [isOver, drop] = useDrop(
+    const [, drop] = useDrop(
         () => ({
             accept: "item",
             drop: (droppedItem) => {
-                console.log("received drop of:", droppedItem);
-                setHoverPosition(HOVER_POSITION.NONE);
-                props.onSwap(droppedItem, ITEM, hoverPosition);
+                console.log("received drop of:", droppedItem, " on:", ITEM);
+                props.onDrop();
             },
             hover: (hoverItem, monitor) => {
                 if (!divRef.current) {
@@ -28,7 +20,6 @@ function DragDropItem(props) {
                 const hoverIndex = ITEM.index;
                 // Don't replace items with themselves
                 if (dragIndex === hoverIndex) {
-                    setHoverPosition(HOVER_POSITION.NONE);
                     return;
                 }
                 // Determine rectangle on screen
@@ -43,12 +34,12 @@ function DragDropItem(props) {
                 // When dragging downwards, only move when the cursor is below 50%
                 // When dragging upwards, only move when the cursor is above 50%
                 // Dragging downwards
-                if (hoverClientY < hoverMiddleY) {
-                    setHoverPosition(HOVER_POSITION.TOP);
+                if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+                    return;
                 }
                 // Dragging upwards
-                if (hoverClientY > hoverMiddleY) {
-                    setHoverPosition(HOVER_POSITION.BOTTOM);
+                if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+                    return;
                 }
                 // Time to actually perform the action
                 // console.log("swap top", hoverItem, "bottom", ITEM);
@@ -58,10 +49,9 @@ function DragDropItem(props) {
                 // but it's good here for the sake of performance
                 // to avoid expensive index searches.
                 // hoverItem.index = hoverIndex;
+                props.onSwap(hoverItem, ITEM);
+                hoverItem.index = hoverIndex;
             },
-            collect: (monitor) => ({
-                isOver: !!monitor.isOver(),
-            }),
         }),
         [ITEM, divRef]
     );
@@ -79,14 +69,12 @@ function DragDropItem(props) {
 
     drag(drop(divRef));
 
-    if (isOver) console.log("isOver ", ITEM);
-
     return (
         <div
             ref={divRef}
             id={props.index}
             style={{
-                opacity: isDragging ? 0 : 1,
+                opacity: props.dragging ? 0 : 1,
                 backgroundColor: "white",
                 color: "black",
                 cursor: "pointer",
@@ -102,6 +90,7 @@ function DragDropItem(props) {
 
 function DnDDemo(props) {
     const [array, setArray] = useState(["A", "B", "C", "D", "E"]);
+    const [dragIndex, setDragIndex] = useState(-1);
     console.log("array", array);
     return (
         <div
@@ -116,28 +105,15 @@ function DnDDemo(props) {
                     index={index}
                     text={arrayElement}
                     key={index}
-                    onSwap={(topItem, bottomItem, hoverPosition) => {
+                    dragging={dragIndex === index}
+                    onDrop={() => setDragIndex(-1)}
+                    onSwap={(topItem, bottomItem) => {
+                        console.log("swapping topItem:", topItem, " bottomItem:", bottomItem);
                         let newArray = [...array];
-                        if (hoverPosition === HOVER_POSITION.TOP) {
-                            newArray.splice(topItem.index, 1);
-                            newArray.splice(
-                                topItem.index < bottomItem.index
-                                    ? bottomItem.index - 1
-                                    : bottomItem.index,
-                                0,
-                                topItem.text
-                            );
-                        } else {
-                            newArray.splice(topItem.index, 1);
-                            newArray.splice(
-                                topItem.index < bottomItem.index
-                                    ? bottomItem.index
-                                    : bottomItem.index + 1,
-                                0,
-                                topItem.text
-                            );
-                        }
-                        console.log("newArray", newArray);
+                        let tempItem = newArray[topItem.index];
+                        newArray[topItem.index] = newArray[bottomItem.index];
+                        newArray[bottomItem.index] = tempItem;
+                        setDragIndex(bottomItem.index);
                         setArray(newArray);
                     }}
                 />
