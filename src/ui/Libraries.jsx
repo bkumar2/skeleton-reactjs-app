fdddc7840148682dd220586784a42e59770e8d21
Bipul Kumar <bipul.kumar@gmail.com>
2022-12-30 10:38:28 +0530

    dnd library

--- src/ui/Libraries.jsx
+++ src/ui/Libraries.jsx
@@ -1,57 +1,59 @@
-import DataTable from 'react-data-table-component';
+import DataTable from "react-data-table-component";
 
 const data = [
     {
         name: "React Router",
-        link: "https://reactrouter.com/web/guides/quick-start"
+        link: "https://reactrouter.com/web/guides/quick-start",
     },
     {
         name: "React Redux",
-        link: "https://react-redux.js.org/introduction/getting-started"
+        link: "https://react-redux.js.org/introduction/getting-started",
     },
     {
         name: "React Bootstrap",
-        link: "https://react-bootstrap.github.io/getting-started/introduction/"
+        link: "https://react-bootstrap.github.io/getting-started/introduction/",
     },
     {
         name: "React Data Table Component",
-        link: "https://github.com/jbetancur/react-data-table-component"
+        link: "https://github.com/jbetancur/react-data-table-component",
     },
     {
         name: "Axios",
-        link: "https://axios-http.com/docs/intro"
+        link: "https://axios-http.com/docs/intro",
     },
     {
         name: "MomentJS",
-        link: "https://momentjs.com/docs/"
+        link: "https://momentjs.com/docs/",
     },
     {
         name: "Styled Components",
-        link: "https://styled-components.com/docs/basics"
+        link: "https://styled-components.com/docs/basics",
     },
-]
+    {
+        name: "React DnD",
+        link: "https://react-dnd.github.io/react-dnd/docs/overview",
+    },
+];
 
 const columns = [
     {
-        name: 'Name',
-        selector: row => row.name,
+        name: "Name",
+        selector: (row) => row.name,
         sortable: true,
     },
     {
-        name: 'Link',
-        selector: row => row.link,
+        name: "Link",
+        selector: (row) => row.link,
         grow: 2,
-        cell: row => <a href={row.link}>{row.link}</a>
+        cell: (row) => <a href={row.link}>{row.link}</a>,
     },
-]
+];
 
 function Libraries(props) {
-    return <div>
-        <DataTable
-            title="Libraries (DataTable Demo)"
-            columns={columns}
-            data={data}
-        />
-    </div>;
+    return (
+        <div>
+            <DataTable title="Libraries (DataTable Demo)" columns={columns} data={data} />
+        </div>
+    );
 }
 export default Libraries;
